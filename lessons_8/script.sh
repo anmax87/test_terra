5fb10495f016d442b086de68218142c7c50ae8f5
anmax <anmax@localhost.localdomain>
2019-12-17 12:13:18 +0200

    modificated script home task 6

--- lessons_8/script.sh
+++ lessons_8/script.sh
@@ -1,27 +1,33 @@
 #! /bin/bash 
+if [ -s avilable_host ] || [ -s error_conn ]; then
+rm avilable_host
+rm error_conn
+fi
+
+file=$(locate -b "\ssh_server.list")
+hostname='(?=^.{1,254}$)(^(?:(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)'
+ip='(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}'
 
 function host_ping {
-cat ssh_server.list | grep -Po '(?=^.{1,254}$)(^(?:(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)' | xargs -r -n1 -I{} -t bash -c 'nc -z {} 22 2> /dev/null && echo {} >> avilable_host'
-cat ssh_server.list | grep -Po '(?=^.{1,254}$)(^(?:(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)' | xargs -r -n1 -I{} -t bash -c 'nc -z {} 22 2> /dev/null || echo {} >> error_conn'
-cat ssh_server.list | grep -Po '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}' | xargs -r -n1 -I{} -t bash -c 'nc -z {} 22 2> /dev/null && echo {} >> avilable_host'
-cat ssh_server.list | grep -Po '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}' | xargs -r -n1 -I{} -t bash -c 'nc -z {} 22 2> /dev/null || echo {} >> error_conn'
+cat $file | grep -Po $hostname | xargs -r -n1 -I{} bash -c 'nc -z {} 22 2> /dev/null && echo {} >> avilable_host | echo {} enable; nc -z {} 22 2> /dev/null || echo {} >> error_conn | echo {} disable'
+cat $file | grep -Po $ip | xargs -r -n1 -I{} bash -c 'nc -z {} 22 2> /dev/null && echo {} >> avilable_host | echo {} enable; nc -z {} 22 2> /dev/null || echo {} >> error_conn | echo {} disable'
 }
 
 function host_info {
 case $1 in
     "mem")
-        cat avilable_host | xargs -r -t -n1 -I{} bash -c 'ssh -i ~/.ssh/terraform_key centos@{} free -h'
+        cat avilable_host | xargs -r -n1 -I{} bash -c 'echo ============ \ ; echo {} \ ; ssh -i ~/.ssh/terraform_key centos@{} free -h'
         ;;
     "cpu")
-        cat avilable_host | xargs -r -t -n1 -I{} bash -c 'ssh -i ~/.ssh/terraform_key centos@{} cat /proc/cpuinfo'
+        cat avilable_host | xargs -r -n1 -I{} bash -c 'echo ============ \ ; echo {} \ ; ssh -i ~/.ssh/terraform_key centos@{} cat /proc/cpuinfo'
         ;;
 esac
 }
 
-if [ -s ssh_server.list ]; then
+if [ -s $file ]; then
 host_ping
 else 
-echo "file is emty or not found"
+echo "file is not exist"
 fi
 
 if [ -s avilable_host ]; then
